8be5c46a20d261d1a9a02bf7b276afa6dc22b18e
Erkinbek <erkinbekabdurakhmanov@gmail.com>
2024-01-21 23:14:29 -0600

    8th day solution

--- day8.py
+++ day8.py
@@ -1,3 +1,13 @@
-n = int(input())
-d = dict(input().split())
-print(d)
+if __name__ == '__main__':
+    n = int(input().strip())
+    dic = {}
+    for i in range(n):
+        name, phone = input().strip().split()
+        dic[name] = int(phone)
+try:
+    while True:
+        query = input().strip()
+        query_result = "Not found" if query not in dic else query + "=" + str(dic[query])
+        print(query_result)
+except EOFError:
+    pass
---
+++ readme.md
@@ -0,0 +1,3 @@
+### Topic: 30 Days of Code
+### Language Python
+### Link to source -> [Hackerrank ](https://www.hackerrank.com/domains/tutorials/30-days-of-code)
